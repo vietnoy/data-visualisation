3d079bf48a91ee8ac237051dc29145df2dea7a80
vietnoy <124616424+vietnoy@users.noreply.github.com>
2023-07-04 12:00:54 +0700

    Add files via upload

---
+++ data visualization project/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const populationButton = document.querySelector('.population');
+// const languageButton = document.querySelector('.languages');
+// const visualisation = document.querySelector('.visualisation');
